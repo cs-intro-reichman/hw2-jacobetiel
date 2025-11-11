dc41f9b1029f31793b9d9d3f7fc74fb9c88f8615
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-11-11 18:36:21 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,23 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String cheer = args[0];
+            int num = Integer.parseInt(args[1]);
+            char c;    
+            String test;               
+            int length = cheer.length(); 
+            String vowels = "AEFHILMNORSX";
+                for (int i=0; i<length-1;i++) {
+                        c = cheer.charAt(i);
+                        if (vowels.indexOf(c) != -1)
+                                System.out.println("Give me an "+cheer.charAt(i)+": " +cheer.charAt(i)+"!" );
+                        else
+                                System.out.println("Give me a  "+cheer.charAt(i)+": " +cheer.charAt(i)+"!" );
+                }
+        System.out.println("What does that spell?");
+        
+        cheer = cheer.toUpperCase();
+        for (int i=0;i<num;i++){
+                System.out.println(cheer+"!!!");
         }
-}
+
+        }}
