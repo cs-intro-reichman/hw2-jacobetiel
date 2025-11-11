2a3ab1629cb2d8bedfee807846c911e666f0e2b3
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-11-11 18:39:42 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,12 +1,13 @@
 public class Cheers {
         public static void main(String[] args) {
 	    String cheer = args[0];
+            cheer = cheer.toUpperCase();
             int num = Integer.parseInt(args[1]);
             char c;    
             String test;               
             int length = cheer.length(); 
             String vowels = "AEFHILMNORSX";
-                for (int i=0; i<length-1;i++) {
+                for (int i=0; i<length;i++) {
                         c = cheer.charAt(i);
                         if (vowels.indexOf(c) != -1)
                                 System.out.println("Give me an "+cheer.charAt(i)+": " +cheer.charAt(i)+"!" );
@@ -15,7 +16,7 @@ public class Cheers {
                 }
         System.out.println("What does that spell?");
         
-        cheer = cheer.toUpperCase();
+        
         for (int i=0;i<num;i++){
                 System.out.println(cheer+"!!!");
         }
