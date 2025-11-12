b3d7d7e0872602b7d09e31b97cce14a15888400c
Jacob Etiel <jacob.etiel@post.runi.ac.il>
2025-11-12 13:09:05 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,23 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		int counter = 0;
+		int deno = 3;
+		double approx = 1;
+		
+		for(int i=1; i<=num;i++ ) {
+			
+			if (i%2 != 0)
+				approx = approx - (1.0/deno);
+			else
+				approx = approx + (1.0/deno);
+			
+				
+			deno = deno+2;
+			System.out.println ("pi according to Java: "+ Math.PI);
+			System.out.println ("pi, approximated:     "+ (approx*4));
+}
+
 	}
 }
